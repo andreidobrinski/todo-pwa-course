473fd87476d22d5dbab6d2deaaa22ccf9ff49947
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-12-14 16:07:22 -0500

    show error when a POST or DELETE is tried in offline

--- src/sw.js
+++ src/sw.js
@@ -15,6 +15,20 @@ self.addEventListener('activate', event => {
   console.log("activate")
 })
 
+self.addEventListener('fetch', event => {
+  if(event.request.method === "POST" || event.request.method === "DELETE") {
+    event.respondWith(
+      fetch(event.request).catch(err => {
+        return new Response(
+          JSON.stringify({ error: "This action disabled while app is offline" }), {
+            headers: { 'Content-Type': 'application/json' }
+          }
+        )
+      })
+    )
+  }
+})
+
 workbox.routing.registerRoute(
   new RegExp('(http|https):.*min\.(css|js)'),
   workbox.strategies.staleWhileRevalidate({
